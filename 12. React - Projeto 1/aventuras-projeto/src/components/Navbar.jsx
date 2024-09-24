549b8446e3835bb10db5207aed20d53d6b0a93ea
CR7sewey <miguelmotacarvalho@gmail.com>
2024-09-24 18:51:36 +0100

    12

--- 12. React - Projeto 1/aventuras-projeto/src/components/Navbar.jsx
+++ 12. React - Projeto 1/aventuras-projeto/src/components/Navbar.jsx
@@ -1,43 +1,28 @@
 import React from "react";
+import logo from "../images/logo.svg";
+import { linksPagina } from "../data";
 
 function Navbar() {
   return (
     <nav className="navbar">
       <div className="nav-center">
         <div className="nav-header">
-          <img src="./images/logo.svg" className="nav-logo" alt="backroads" />
+          <img src={logo} className="nav-logo" alt="backroads" />
           <button type="button" className="nav-toggle" id="nav-toggle">
             <i className="fas fa-bars"></i>
           </button>
         </div>
         <ul className="nav-links" id="nav-links">
-          <li>
-            <a href="#home" className="nav-link">
-              {" "}
-              home{" "}
-            </a>
-          </li>
-
-          <li>
-            <a href="#about" className="nav-link">
-              {" "}
-              about{" "}
-            </a>
-          </li>
-
-          <li>
-            <a href="#services" className="nav-link">
-              {" "}
-              services{" "}
-            </a>
-          </li>
-
-          <li>
-            <a href="#tours" className="nav-link">
-              {" "}
-              tours
-            </a>
-          </li>
+          {linksPagina.map((link) => {
+            const { id, href, text } = link;
+            return (
+              <li key={id}>
+                <a href={href} className="nav-link">
+                  {text}
+                </a>
+              </li>
+            );
+          })}
         </ul>
 
         <ul className="nav-icons">
